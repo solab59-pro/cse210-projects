a37ffde542251c1a355e63c29803743e50770fa6
solab59-pro <jaywisefreeworld59@gmail.com>
2025-05-15 18:34:22 +0100

    Created job and resume

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._companyName = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2024;
+        job1._endYear = 2026;
+
+        Job job2 = new Job();
+        job2._companyName = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2012;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "John Solomon Ekaette";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
