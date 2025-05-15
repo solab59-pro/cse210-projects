6320a76ec1416e4993f1598079401a26f5d89fb0
solab59-pro <jaywisefreeworld59@gmail.com>
2025-05-15 19:17:26 +0100

    week1 exercise done

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is the percentage of your grade? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string alphabet = "";
+
+        if (percent >= 90)
+        {
+            alphabet = "A";
+        }
+        else if (percent >= 80)
+        {
+            alphabet = "B";
+        }
+        else if (percent >= 70)
+        {
+            alphabet = "C";
+        }
+        else if (percent >= 60)
+        {
+            alphabet = "D";
+        }
+        else
+        {
+            alphabet = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {alphabet}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,55 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        
+        
+        int userInputNumber = -1;
+        while (userInputNumber != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            
+            string userResponse = Console.ReadLine();
+            userInputNumber = int.Parse(userResponse);
+            
+            // Only add the number to the list if it is not 0
+            if (userInputNumber != 0)
+            {
+                numbers.Add(userInputNumber);
+            }
+        }
+
+// computing the sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+       //finding the average 
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // Part 3: Find the max
+
+        
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                // if this number is greater than the max, we have found the new max!
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
